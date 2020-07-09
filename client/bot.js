2417b01b76847cb8b8a1e0eaf55ebd66420238f3
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-07-09 08:39:27 +0700

    Update bot.js

--- client/bot.js
+++ client/bot.js
@@ -5,7 +5,26 @@ const client = new discord.Client();
 
 const { Client, Schema } = require('klasa');
 
+Client.defaultPermissionLevels
+    .add(8, ({ c, author }) => config.discord.admin_role_id.split(' ').includes(author.id));
 
+const client = new Client({
+    commandEditing: true,
+    prefix: config.bot.prefix,
+    production: true,
+    consoleEvents: {
+        log: false,
+        error: false,
+        warn: false
+    },
+    disabledCorePieces: ["commands"]
+});
+
+module.exports.init = async (token) => {
+    client.userBaseDirectory = __dirname;    
+    client.login(token);
+    return client;
+}
 
 client.on('ready', (message) => {
     console.log(`Logged In As ${client.user.username}`)
@@ -19,5 +38,3 @@ client.on('message', (message) => {
     }
 });
 
-
-client.login(config.discord.token);
