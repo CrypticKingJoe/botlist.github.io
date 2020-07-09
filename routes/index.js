73166a74db3b84b453786be3a17c377e696affb6
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-07-09 21:38:02 +0700

    create index.js in the routes folder

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+
+const bots = require("./bots/index");
+const api = require("./api/index");
+const theme = require("./theme");
+
+const join = require("./join");
+const login = require("./login");
+const logout = require("./logout");
+const user = require("./user");
+const me = require("./me");
+
+const route = Router();
+
+route.use("/bots", bots);
+route.use("/api", api);
+route.use("/theme", theme);
+
+route.use("/join", join);
+route.use("/logout", logout);
+route.use("/login", login);
+route.use("/user", user);
+route.use("/me", me);
+
+
+route.get('/', (req, res) => {
+    if (!req.query.q) res.render('index');
+    else res.redirect(`/bots/search?q=${encodeURIComponent(req.query.q)}`)
+});
+
+module.exports = route;
