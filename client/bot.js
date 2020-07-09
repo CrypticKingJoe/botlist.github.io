9d915f423d7de9fcab30a49cb5c43488c9f52e2c
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-07-09 09:18:14 +0700

    add ping command

--- client/bot.js
+++ client/bot.js
@@ -1,7 +1,11 @@
 const discord = require ('discord.js');
 const config = require('../config');
 const client = new discord.Client();
-
+const { Client, Collection } = require("discord.js");
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+client.event = new Collection();
 
 const { Client, Schema } = require('klasa');
 
@@ -33,8 +37,20 @@ client.on('ready', (message) => {
 
 client.on('message', (message) => {
     let prefix = config.bot.prefix;
-    if(message.content === prefix + ' ping') {
-        message.reply('PONG!!!');
-    }
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    if (command) 
+        command.run(client, message, args);
 });
 
