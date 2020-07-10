8fc7f137ef0bbfaad6c455a1ec50f8899dbe0eb2
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-07-10 12:07:41 +0700

    add css

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: 8000,
+    port: 80,
 
     language: 'en-us',
 
@@ -8,18 +8,28 @@ module.exports = {
     discord: {
         client_id: '730219913951707186',
         client_secret: 'ZoYIxhxC2_tpzyL9RRYM_n55QfOs1ty6',
-        token: 'NzMwMjE5OTEzOTUxNzA3MTg2.XwZyTw.XSZOHowuKGREPSwbQJ1GFX9BgtI',
+        token: 'NzMwMjE5OTEzOTUxNzA3MTg2.XwadIA.bXkU8bMRLifczmfy6nHvVM2dTVA',
         notification: true,
         guild_id: '730063771778678829',
         mod_role_id: '730223401607888986',
-        admin_role_id: '730226711823646770'
+        admin_role_id: '730226711823646770',
+        BOT_ROLE_ID: '730578235209023688',
+        UNVERIFIED_ROLE_ID: '730655812606885970',
+        ADMIN_USER: '332109764286742529',
+        log_id: '730230291687276604',
+        mod_log_id: '730223337967452230',
+        VERIFY_ROLES_ID: '730749423876964454',
+        UNMUTED_ROLES_ID: '730749679016607815',
+        BOT_DEV_ROLE: '730709935851307049',
+        GUILD_INVITE: 'https://discord.gg/5pVRxjR'
     },
 
     database: {
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'botlist'
+        database: 'botlist',
+        MONGO_DB_URL: 'mongodb://localhost:27017/mydb'
     },
 
     bot: {
