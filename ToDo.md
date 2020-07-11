6636a0f608f5d6d77870e29aca92b90ceb618daf
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-07-11 08:11:19 +0700

    add todo list and updated the index.js

---
+++ ToDo.md
@@ -0,0 +1,5 @@
+# To Do List
+
+[x] Make A Router Folder
+[x] Make A Index.html
+[x] Make Users And Bots Model
--- index.js
+++ index.js
@@ -9,21 +9,20 @@ var createError = require('http-error');
 var app = express();
 const colors = require('colors');
 const App = require('./structures/app.js');
-const { PORT, DISCORD_TOKEN, MONGO_DB_URL } = process.env;
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
 const bot = require('./client/bot');
 
-let botname = "Muge";
+  let botname = "Muge";
+
+    //CONNECT TO THE DATABASE
     mongoose.connect(`${config.database.MONGO_DB_URL}`, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
     console.log(colors.yellow(`Connected to the database on `) + colors.underline.green(MONGO_DB_URL));
+    //Logged In The Bot
     let client = bot.init(config.discord.token);
     console.log(colors.yellow(`Logged in as `) + colors.underline.green(botname));
-     new App(client).listen(PORT || 8080);
-    console.log(colors.yellow(`Running on port `) + colors.underline.green(PORT || 8080));
+    //Listen To The Server Port
+     new App(client).listen(config.port || 8080);
+    console.log(colors.yellow(`Running on port `) + colors.underline.green(config.port || 8080));
